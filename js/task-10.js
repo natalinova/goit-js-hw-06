9f430a84931c80b98c3e2b967cd22f1f39bd8cbd
Nataliya Novak <novak.natali@gmail.com>
2022-06-26 01:39:42 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,7 +1,7 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const allConst = {
+const allConst = { // створено загальний об єкт для всіх const - Репета
   commonBlock: document.querySelector('#controls'),//подія на загальному div
   input: document.querySelector('input'),//форма вводу кількості елементів
   create: document.querySelector('[data-create]'),//створення div
@@ -12,13 +12,25 @@ allConst.input.addEventListener('input', onInputNumber); // слухач input
 function onInputNumber(event) {//
   const amount = event.currentTarget.value
   console.log(amount); // щоб побачити що виводиться правильне число - правильне, ок
-  return amount // ну, я повертаю значення але це неправильно
+  return amount // я повертаю значення але це неправильно
 }
 
 allConst.create.addEventListener('click', createBoxes) 
-function createBoxes(count) { // от я не знаю як вставити значення з попередньої функції сюди! 
+function createBoxes(amount) { // от я не знаю як вставити значення з попередньої функції сюди! 
 // логічно, що не можу вставити - вона ж визначена в іншій функції а не глобально :(
-  
-  console.log(count) //
+  let arrayBlock = [];
+  for (i = 0; i < amount; i += 1) {
+    const newItem = document.createElement('div');
+    newItem.style.backgroundColor = getRandomHexColor();
+    newItem.style.width = '(30 + (i * 10))px';
+    newItem.style.width = '(30 + i * 10)px';
+    arrayBlock.push(newItem)
+  }
+  allConst.commonBlock.append(...arrayBlock);
 }
 
+allConst.destroy.addEventListener('click', destroyBlock)
+function destroyBlock() {
+  const arrayDestroy = allConst.commonBlock.querySelectorAll('div')
+  arrayDestroy.remove()
+}
